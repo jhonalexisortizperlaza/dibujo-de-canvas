df0c255b6572db673cc25d73bbfe4f2fec2a547c
jhonalexisortizperlaza <jhonalexisortizperlaza595@gmail.com>
2021-12-29 16:36:02 -0500

    se agrega el proyecto

---
+++ codigo.js
@@ -0,0 +1,37 @@
+let texto = document.getElementById("texto_lineas");
+let boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick );
+
+let d = document.getElementById("dibujito");
+let ancho = d.width;
+let lienzo = d.getContext("2d");
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+function dibujoPorClick()
+{
+  let lineas = parseInt(texto.value);
+  let l = 0;
+  let yi, xf;
+  let colorcito = "#FAA";
+  let espacio = ancho / lineas;
+
+  for(l = 0; l < lineas; l++)
+  {
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    console.log("Linea " + l);
+  }
+
+  dibujarLinea(colorcito, 1,1,1,299);
+  dibujarLinea(colorcito, 1,299,299,299);
+}
